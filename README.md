2c26dd04d4a57412dea663bf6f6633a428f57952
rafaellolufas <166717631+rafaellolufas@users.noreply.github.com>
2025-04-11 23:31:18 +0000

    altera todos os arquivos do projeto

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # programa-o-3c
+## Sobre
+Refatoração de um site implementando recursos de acessibilidade no HTML, CSS e JS.
+## Recursos de acessibilidade
+- Atributos aria
+- Alt
+- Tab-index
+- Menu de acessibilidade
+## Tecnologias utilizadas
+- Bootstrap
+- ScrollReveal.js
+- HTML
+- CSS
+- JS
--- script.js
+++ script.js
@@ -15,8 +15,17 @@ document.addEventListener('DOMContentLoaded', function(){
         documen.body.style.fontSize = '${tamanhoAtualFonte}rem';
     });
 });
-
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
 botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 })
+const alternaContraste = document.getElementById('alterna-contraste')
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
